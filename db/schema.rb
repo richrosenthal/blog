c5a87d3c8940c6ab96d496aadb6f1d0f062dee65
richrosenthal <rrosenthal2013@gmail.com>
2020-04-23 12:23:21 -0400

    new migration add body column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_093647) do
+ActiveRecord::Schema.define(version: 2020_04_23_003205) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
